1aaf86ab429abd225a5db83ad1f8031b80f8ead6
amitbhati9773 <151907744+amitbhati9773@users.noreply.github.com>
2024-12-17 11:24:48 +0530

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,5 +1,17 @@
-var http = require('http');
-http.createServer(function (req, res) {
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.write("I'm alive");
+//   res.end();
+// }).listen(8080);
+
+
+import http from 'http';
+
+const server = http.createServer((req, res) => {
   res.write("I'm alive");
   res.end();
-}).listen(8080);
+});
+
+server.listen(8080);
+
+export default server; // Add this line
